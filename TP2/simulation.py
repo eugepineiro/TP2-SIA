a7ed628aff987c49ee4dcd02b74a3cff4bfef96f
euge <epineiro@itba.edu.ar>
2021-03-31 15:14:15 -0300

    deleted comments

--- TP2/simulation.py
+++ TP2/simulation.py
@@ -34,14 +34,6 @@ def get_diversity(characters):
     size = len(characters)
     return (diff / ((size-1)*size)/2) * 100
 
-#def get_diversity_by_stats(characters): #dos individuos son iguales si tienen la misma fuerza y la misma pericia y agilidad etc con un delta
-    
-    
-#def get_diversity_by_fitness(characters): #dos individuos son iguales si tienen el mismo fitness con un delta
-    
-    
-    
-
 def max_fitness(characters):
     return max(list(map(lambda character: character.fitness, characters)))
 
@@ -137,73 +129,36 @@ def cutting(data_handler, item_handler, characters, iteration_func): # cutting_m
     for character in final_characters[1:]:
         if character.fitness > best.fitness:
             best = character
+    print("Best character is")
     print(best)
+    print("'Height': %g" %best.height)
     for item in best.equipment.items():
         print(item)
     graph.show()
 
 def runIteration(data_handler, item_handler, characters, plotter, generation):
 
-    # population_amount = data['population_amount']
-    # character_class = data['class']
-    # individuals_amount = data['individuals_amount']
-    # individual_mutation_probability = data['individual_mutation_probability']
-    # selection_method_a = data["methods"]["selection_a"]
-    # selection_method_b = data["methods"]["selection_b"]
-    # mutation_method = data["methods"]["mutation"]
-    # replacement_a = data["methods"]["replacement_a"]
-    # replacement_b = data["methods"]["replacement_b"]
-    # selection_prob = data["methods"]["selection_prob"]
-    # replacement_prob = data["methods"]["replacement_prob"]
-    # implementation = data["implementation"]
-    # crossover_func = data["methods"]["crossover"] 
-    # threshold = data["methods"]["selection_params"]["p_tournaments_threshold"] 
-
     # Parents Selection 
-    print("-------------------- SELECTION ----------------------")
     first_cut = math.floor(data_handler.individuals_amount*data_handler.selection_prob)
     second_cut = math.ceil(data_handler.individuals_amount*(1-data_handler.selection_prob))
     parents1 = data_handler.selection(data_handler.selection_method_a, characters, first_cut, data_handler.population_amount, generation+1)
     parents2 = data_handler.selection(data_handler.selection_method_b, characters, second_cut, data_handler.population_amount, generation+1)
     parents = parents1 + parents2
-    # print(parents)
-    # print(len(parents))
     
     # Pair parent for crossover 
     parents1 = parents[0::2]
     parents2 = parents[1::2] 
+    
     # Crossover --> get children  
-    print("-------------------- CROSSOVER ----------------------")
-
     children = data_handler.crossover(parents1, parents2)
-    # print(children)
-
-
+   
     # Mutate children (para cada hijo chequeo --> si cumple con Pm --> lo muto, sino sigo)
-    print("-------------------- MUTATION ----------------------")
-    min_fitness = 20
-    # for c in children :
-        # if c.fitness < min_fitness :
-            # min_fitness = c.fitness
-    # print("MIN FITNESS BEFORE MUTATION: %d" %min_fitness)
-        
-    #print(children)
     for j,individual in enumerate(children):
         if data_handler.individual_mutation_probability > MutationLib.getMutationProbability():
             children[j] = data_handler.mutation(individual, item_handler)
-    #print(children)
-    min_fitness = 20
-    # for c in children :
-        # if c.fitness < min_fitness :
-            # min_fitness = c.fitness
-    # print("MIN FITNESS AFTER MUTATION: %d" %min_fitness)
-
+ 
     # Get new Generation
-    print("-------------------- REPLACEMENT ----------------------")
-    print(len(characters))
-
     characters = replacement(data_handler, characters, children, generation=generation+1)
-    print(len(characters))
     plotter.update_plots(generation + 1,min(map(lambda character: character.fitness,characters)),avg_fitness(characters),get_diversity(characters),max(map(lambda character: character.fitness,characters)))
 
     return characters
